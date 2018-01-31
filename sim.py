c345d595c5bd1788a046355d17fed075c293898e
jbrophy <jonathanbrophy47@gmail.com>
2018-01-31 13:45:56 -0800

    fixed similarity in smaller chunks

--- sim.py
+++ sim.py
@@ -1,8 +1,14 @@
 import re
+import sys
 import pandas as pd
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def out(message=''):
+    sys.stdout.write(message + '\n')
+    sys.stdout.flush()
 
 
 def ngrams(string, n=3):
@@ -17,32 +23,29 @@ def tf_idf(strings, analyzer='word'):
     return tf_idf_matrix
 
 
-def find_similarities(strings, sim_thresh=0.8, max_text_id=0):
-    print('creating tf-idf matrix...')
+def find_similarities(df, strings, sim_thresh=0.8, max_text_id=0):
+    out('creating tf-idf matrix...')
     tf_idf_matrix = tf_idf(strings, analyzer=ngrams)
 
-    print('computing cosine similarities...')
+    out('computing cosine similarities...')
     cos_sim = cosine_similarity(tf_idf_matrix)
 
-    print('filtering out simiarities below threshold...')
+    out('filtering out simiarities below threshold...')
     cos_sim[cos_sim < sim_thresh] = 0.0
 
-    print('converting matrix to sparse matrix...')
+    out('converting matrix to sparse matrix...')
     scm = sparse.csr_matrix(cos_sim)
 
-    print('putting matches into groups...')
+    out('putting matches into groups...')
     groups = {-1: set()}
-    explored = set()
     i = max_text_id + 1
 
     # TODO: put in a separate method
     for ndx in range(len(strings)):
-        explored.add(ndx)
         matches = set(scm[ndx].indices)
         group = set(matches)
 
         if len(group) > 1:
-            explored.update(matches)
             groups[i] = group
             i += 1
         else:
@@ -64,23 +67,25 @@ def find_similarities(strings, sim_thresh=0.8, max_text_id=0):
     temp_df = temp_df.sort_index()
     temp_df = temp_df[temp_df['text_id'] != -1]
     max_text_id = max(temp_df['text_id'])
-    # temp_df.to_csv('sim.csv', index=None)
     return temp_df, max_text_id
 
 if __name__ == '__main__':
-    print('extracting messages...')
+    out('extracting messages...')
     df = pd.read_csv('independent/data/toxic/comments.csv', nrows=None)
     df['len'] = df['text'].str.len()
-    df['len_id'] = pd.qcut(df['len'], 5).cat.codes
+    df['len_id'] = pd.qcut(df['len'], 10).cat.codes
 
     sim_chunks = []
     max_text_id = 0
-    for len_id in range(5):
-        print('len_id: %d' % (len_id))
+    for len_id in range(10):
+        out('\nlen_id: %d' % (len_id))
         df_chunk = df[df['len_id'] == len_id]
+        df_chunk = df_chunk.reset_index().drop(['index'], axis=1)
+        # out(str(len(df_chunk)))
         strings = list(df_chunk['text'])
-        sim_chunk_df, max_text_id = find_similarities(strings, max_text_id)
+        sim_chunk_df, max_text_id = find_similarities(df_chunk, strings,
+                max_text_id=max_text_id)
         sim_chunks.append(sim_chunk_df)
     sim_df = pd.concat(sim_chunks)
     sim_df.to_csv('sim.csv', index=None)
-    print(df)
+    out(str(sim_df))
