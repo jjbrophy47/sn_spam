1d6a78c4d3e86e140afd0217dca61c017d9b97dc
jbrophy <jonathanbrophy47@gmail.com>
2018-02-15 10:41:23 -0800

    fixing big_aupr again domain

--- one_off/big_aupr.py
+++ one_off/big_aupr.py
@@ -2,17 +2,15 @@ import os
 import pandas as pd
 from sklearn.metrics import average_precision_score
 
-domain = 'twitter'
+domain = 'youtube'
 ind_data_dir = 'independent/data/' + domain + '/'
 fold_dir = 'independent/data/' + domain + '/folds/'
 ind_preds_dir = 'independent/output/' + domain + '/predictions/'
 rel_preds_dir = 'relational/output/' + domain + '/predictions/'
 
-os.system('cat %stest_*.csv > test.csv' % (fold_dir))
+# os.system('cat %stest_*.csv > test.csv' % (fold_dir))
 os.system('cat %stest_*_preds.csv > ind_preds.csv' % (ind_preds_dir))
-print('cat %smrf_preds_*.csv > mrf_preds.csv' % (rel_preds_dir))
 os.system('cat %smrf_preds_*.csv > mrf_preds.csv' % (rel_preds_dir))
-print('cat %spsl_preds_*.csv > psl_preds.csv' % (rel_preds_dir))
 os.system('cat %spsl_preds_*.csv > psl_preds.csv' % (rel_preds_dir))
 
 labels_df = pd.read_csv(ind_data_dir + 'comments.csv')
