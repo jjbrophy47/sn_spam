057d9505d7358d6b961700941e218039d7414f38
jbrophy <jonathanbrophy47@gmail.com>
2018-02-15 10:13:13 -0800

    changing big_aupr script to use raad in all labels

--- one_off/big_aupr.py
+++ one_off/big_aupr.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.metrics import average_precision_score
 
 domain = 'twitter'
+ind_data_dir = 'independent/data/' + domain + '/'
 fold_dir = 'independent/data/' + domain + '/folds/'
 ind_preds_dir = 'independent/output/' + domain + '/predictions/'
 rel_preds_dir = 'relational/output/' + domain + '/predictions/'
@@ -12,7 +13,7 @@ os.system('cat %stest_preds_*.csv > ind_preds.csv' % (ind_preds_dir))
 os.system('cat %smrf_preds_*.csv > mrf_preds.csv' % (rel_preds_dir))
 os.system('cat %spsl_preds_*.csv > psl_preds.csv' % (rel_preds_dir))
 
-labels_df = pd.read_csv('test.csv')
+labels_df = pd.read_csv(ind_data_dir + 'comments.csv')
 ind_df = pd.read_csv('ind_preds.csv')
 mrf_df = pd.read_csv('mrf_preds.csv')
 psl_df = pd.read_csv('psl_preds.csv')
