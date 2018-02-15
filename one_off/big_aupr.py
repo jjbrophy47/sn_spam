e8a564f4ba77c71c29ff2739e73a9990a9fd7911
jbrophy <jonathanbrophy47@gmail.com>
2018-02-15 10:27:42 -0800

    fixing big_aupr again

--- one_off/big_aupr.py
+++ one_off/big_aupr.py
@@ -10,7 +10,9 @@ rel_preds_dir = 'relational/output/' + domain + '/predictions/'
 
 os.system('cat %stest_*.csv > test.csv' % (fold_dir))
 os.system('cat %stest_*_preds.csv > ind_preds.csv' % (ind_preds_dir))
+print('cat %smrf_preds_*.csv > mrf_preds.csv' % (rel_preds_dir))
 os.system('cat %smrf_preds_*.csv > mrf_preds.csv' % (rel_preds_dir))
+print('cat %spsl_preds_*.csv > psl_preds.csv' % (rel_preds_dir))
 os.system('cat %spsl_preds_*.csv > psl_preds.csv' % (rel_preds_dir))
 
 labels_df = pd.read_csv(ind_data_dir + 'comments.csv')
@@ -19,10 +21,10 @@ mrf_df = pd.read_csv('mrf_preds.csv')
 psl_df = pd.read_csv('psl_preds.csv')
 
 df = labels_df.merge(ind_df).merge(psl_df).merge(mrf_df)
-df = df[['com_id', 'label', 'ind_pred', 'psl_pred', 'mrf_pred']]
+df = df[['com_id', 'label', 'ind_pred', 'rel_pred', 'mrf_pred']]
 
 # TODO: add tiny bit of noise?
 
-for model in ['ind_pred', 'psl_pred', 'mrf_pred']:
+for model in ['ind_pred', 'rel_pred', 'mrf_pred']:
     aupr = average_precision_score(df['label'], df[model])
     print('%s aupr: %.4f' % (model, aupr))
