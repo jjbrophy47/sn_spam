3852648d102b27c5ddcc5f4489e7d9fca771404b
Jonathan Brophy <jonathanbrophy47@gmail.com>
2018-03-03 16:07:20 -0800

    added mean and std_dev to big_aupr

--- one_off/big_aupr.py
+++ one_off/big_aupr.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from sklearn.metrics import average_precision_score
 
@@ -29,16 +30,30 @@ for i in range(exp_start, exp_start + num_exps):
 
 labels_df = pd.read_csv(ind_data_dir + 'comments.csv')
 labels_df = labels_df[['com_id', 'label']]
+
 ind_df = pd.concat(ind)
 mrf_df = pd.concat(mrf)
 psl_df = pd.concat(psl)
-
-df = labels_df.merge(ind_df).merge(mrf_df).merge(psl_df)
 
 print(df)
 
 # TODO: add tiny bit of noise?
 
+def compute_mean_aupr(dfs, labels_df, model='ind_pred'):
+    auprs = []
+    for sample_df in dfs:
+        df = labels_df.merge(sample_df)
+        aupr = average_precision_score(df['label'], df[model])
+        auprs.append(aupr)
+    t = (model, len(dfs), np.mean(auprs), np.std(auprs))
+    print('%s: num test sets: %d, mean aupr: %.4f +/- %.4f' % t)
+
+l = [(ind_df, 'ind_pred', mrf_df, 'mrf_pred', psl_df, 'psl_pred')]
+for dfs, model in l:
+    compute_mean_aupr(dfs, labels_df, model=model)
+
+# compute combined test set aupr
+df = labels_df.merge(ind_df).merge(mrf_df).merge(psl_df)
 for model in ['ind_pred', 'rel_pred', 'mrf_pred']:
     aupr = average_precision_score(df['label'], df[model])
     print('%s aupr: %.4f' % (model, aupr))
