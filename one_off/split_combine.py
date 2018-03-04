eead707d6d66994c51b2cac7afc3fd8228bb54d6
Jonathan Brophy <jbrophy@bella.cs.uoregon.edu>
2018-03-04 13:27:21 -0800

    fixing split_combine script

--- one_off/split_combine.py
+++ one_off/split_combine.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def split(df, num_chunks=80, out_dir='', out_fname='chunk_'):
 
-    chunk_size = int(len(df) / 80)
+    chunk_size = int(len(df) / num_chunks)
     chunk_start = 0
 
     for i in range(num_chunks):
@@ -25,6 +25,7 @@ def combine(in_dir='', in_fname='replace_', num_chunks=80, out_dir='',
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('independent/data/twitter/comments.csv')
-    split(df, num_chunks=80)
-    combine(in_dir='chunks/', num_chunks=80)
+    data_dir = '/Volumes/Brophy/data/twitter/'
+    df = pd.read_csv(data_dir + 'comments.csv')
+    split(df, num_chunks=5, out_dir=data_dir + 'chunks/')
+    # combine(in_dir='chunks/', num_chunks=80)
