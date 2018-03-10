27556e6ed3b1b5bb7bf23ffd384e1a85b32fb288
jbrophy <jonathanbrophy47@gmail.com>
2018-03-10 14:37:18 -0800

    made cosin similarity more scalable using a sparse output

--- one_off/sim.py
+++ one_off/sim.py
@@ -3,47 +3,12 @@ import sys
 import numpy as np
 import pandas as pd
 from collections import defaultdict
-from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 
 
 # public
-def similarities(df, num_chunks=10, target_col='text', out_col='text_id',
-                 out_dir='', fname='sim.csv'):
-    _out('extracting messages...')
-
-    if num_chunks == 1:
-        max_id = 0
-        df = df.reset_index().drop(['index'], axis=1)
-        strings = list(df[target_col])
-        sim_df, max_id = cosine_similarities(df, strings, max_id=max_id,
-                                             output_col=out_col)
-
-    else:
-        df['len'] = df[target_col].str.len()
-        df['len_id'] = pd.qcut(df['len'], num_chunks,
-                               duplicates='drop').cat.codes
-
-        sim_chunks = []
-        max_id = 0
-        for len_id in range(df['len_id'].min(), df['len_id'].max()):
-            df_chunk = df[df['len_id'] == len_id]
-            _out('\nlen_id: %d, size: %d' % (len_id, len(df_chunk)))
-
-            df_chunk = df_chunk.reset_index().drop(['index'], axis=1)
-            strings = list(df_chunk[target_col])
-            sim_chunk_df, max_text_id = cosine_similarities(df_chunk, strings,
-                                                            max_id=max_id,
-                                                            output_col=out_col)
-            sim_chunks.append(sim_chunk_df)
-        sim_df = pd.concat(sim_chunks)
-
-    sim_df.to_csv(out_dir + fname, index=None)
-    _out(str(sim_df))
-
-
 def knn_similarities(df, sim_thresh=0.8, n_neighbors=100,
                      approx_datapoints=120000, max_feats=None,
                      in_col='text', out_col='text_id', out_dir='',
@@ -90,51 +55,41 @@ def knn_similarities(df, sim_thresh=0.8, n_neighbors=100,
     sim_df.to_csv(out_dir + fname, index=None)
 
 
-def cosine_similarities(df, strings, sim_thresh=0.8, max_id=0,
-                        output_col='text_id', n_neighbors=100):
-    _out('creating tf-idf matrix...')
-    tf_idf_matrix = _tf_idf(strings, analyzer=_ngrams)
+def cosine_similarities(df, sim_thresh=0.8, in_col='text',
+                        out_col='text_id', approx_datapoints=120000,
+                        max_feats=None, out_dir='', fname='sim.csv'):
+    group_id = 0
+    all_ids = defaultdict(set)
+    dfs = _split_data(df, approx_datapoints=approx_datapoints, in_col=in_col)
 
-    _out('computing cosine similarities...')
-    cos_sim = cosine_similarity(tf_idf_matrix)
+    for n, chunk_df in enumerate(dfs):
+        _out('creating tf-idf matrix for chunk %d...' % n)
+        groups = defaultdict(set)
+        g_df = chunk_df.groupby(in_col).size().reset_index()
+        strings = list(g_df[in_col])
+        tf_idf_matrix = _tf_idf(strings, analyzer=_ngrams, max_feats=max_feats)
+        _out(str(tf_idf_matrix.shape))
 
-    _out('filtering out simiarities below threshold...')
-    cos_sim[cos_sim < sim_thresh] = 0.0
+        _out('computing cosine similarities...')
+        cos_sim = cosine_similarity(tf_idf_matrix, dense_output=False)
 
-    _out('converting matrix to sparse matrix...')
-    scm = sparse.csr_matrix(cos_sim)
+        _out('filtering out simiarities below threshold...')
+        scm = cos_sim >= sim_thresh
 
-    _out('putting matches into groups...')
-    groups = {-1: set()}
-    i = max_id + 1
+        _out('putting matches into groups...')
+        for ndx in range(len(strings)):
+            groups[group_id].update(set(scm[ndx].indices))
+            group_id += 1
 
-    for ndx in range(len(strings)):
-        matches = set(scm[ndx].indices)
-        group = set(matches)
+        groups = _merge_identical_groups(groups)
+        ids = _assign_ids_to_items(groups, strings)
+        all_ids = _aggregate_identical_keys(all_ids, ids)
 
-        if len(group) > 1:
-            groups[i] = group
-            i += 1
-        else:
-            groups[-1].update(group)
-
-    rows = []
-    indices = []
-    for group_id, ndxs in groups.items():
-        indices.extend(ndxs)
-        rows.extend([group_id] * len(ndxs))
-
-    r = []
-    msgs = list(zip(indices, rows))
-    for ndx, group_id in msgs:
-        msg_id = df.loc[ndx]['com_id']
-        r.append((msg_id, group_id))
-
-    temp_df = pd.DataFrame(r, columns=['com_id', output_col], index=indices)
-    temp_df = temp_df.sort_index()
-    temp_df = temp_df[temp_df[output_col] != -1]
-    max_id = max(temp_df[output_col])
-    return temp_df, max_id
+    all_ids = _prune_single_items(all_ids, df, in_col)
+    all_ids = _prune_redundant_ids(all_ids)
+    print(all_ids)
+    sim_df = _ids_to_dataframe(all_ids, df, in_col=in_col, out_col=out_col)
+    sim_df.to_csv(out_dir + fname, index=None)
 
 
 # private
@@ -216,7 +171,7 @@ def _prune_single_items(all_ids, df, in_col):
     g1 = list(g_df[g_df[0] == 1][in_col])
 
     for key in g1:
-        if len(all_ids[key]) == 1:
+        if len(all_ids[key]) == 0 or len(all_ids[key]) == 1:
             all_ids.pop(key)
 
     return all_ids
@@ -261,8 +216,10 @@ if __name__ == '__main__':
     in_dir = 'independent/data/' + domain + '/'
     df = pd.read_csv(in_dir + info_type + '.csv')
 
-    # similarities(df, num_chunks=1, target_col=info_type,
-    #              out_col=info_type + '_id')
-    knn_similarities(df, in_col=info_type, out_col=info_type + '_id',
-                     out_dir=in_dir, fname=info_type + '_sim.csv',
-                     max_feats=None, approx_datapoints=120000)
+    cosine_similarities(df, in_col=info_type, out_col=info_type + '_id',
+                        out_dir=in_dir, fname=info_type + '_sim.csv',
+                        max_feats=None, approx_datapoints=120000,
+                        sim_thresh=0.7)
+    # knn_similarities(df, in_col=info_type, out_col=info_type + '_id',
+    #                  out_dir=in_dir, fname=info_type + '_sim.csv',
+    #                  max_feats=None, approx_datapoints=120000)
