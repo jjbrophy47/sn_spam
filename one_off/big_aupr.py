8094b26bbeb7df37a6376743004cf8e7603e1c51
jbrophy <jonathanbrophy47@gmail.com>
2018-03-16 10:27:40 -0700

    changing the way mean aupr is computed

--- one_off/big_aupr.py
+++ one_off/big_aupr.py
@@ -38,20 +38,35 @@ def compute_big_aupr(start_fold=0, num_folds=5, domain='twitter'):
         ind_df = pd.concat(ind)
         df = df.merge(ind_df)
         models.append('ind_pred')
+        _compute_mean(ind, df, 'ind_pred')
     if len(mrf) > 0:
         mrf_df = pd.concat(mrf)
         df = df.merge(mrf_df)
         models.append('mrf_pred')
+        _compute_mean(mrf, df, 'mrf_pred')
     if len(psl) > 0:
         psl_df = pd.concat(psl)
         df = df.merge(psl_df)
         models.append('psl_pred')
+        _compute_mean(psl, df, 'psl_pred')
 
     print(df.head(5))
 
     for model in models:
         aupr = average_precision_score(df['label'], df[model])
         print('%s aupr: %.4f' % (model, aupr))
+
+
+def _compute_mean(model_dfs, df, col='ind_pred'):
+    auprs = 0
+
+    for model_df in model_dfs:
+        qf = df.merge(model_df)
+        aupr = average_precision_score(qf['label'], qf[col])
+        if not pd.isnull(aupr) or aupr != 1.0:
+            auprs.append(aupr)
+    t = (col, len(aupr), np.mean(auprs), np.std(auprs))
+    print('%s: num test sets: %d, mean aupr: %.4f +/- %.4f' % t)
 
 
 def compute_mean_aupr(domain='twitter', data='both'):
@@ -70,5 +85,5 @@ def compute_mean_aupr(domain='twitter', data='both'):
 
 if __name__ == '__main__':
     domain = 'twitter'
-    compute_big_aupr(start_fold=0, num_folds=5, domain=domain)
-    compute_mean_aupr(domain=domain, data='ind')
+    compute_big_aupr(start_fold=0, num_folds=185, domain=domain)
+    compute_mean_aupr(domain=domain, data='both')
