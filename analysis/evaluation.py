d4cc5cccc1b4aebd063493cd84f06756624ace02
jbrophy <jonathanbrophy47@gmail.com>
2018-04-01 19:28:06 -0700

    cleaning up evaluation script and chaning noise limit to 0.000025

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -23,7 +23,6 @@ class Evaluation:
 
         self.settings()
         data_f, ind_pred_f, rel_pred_f, image_f, status_f = self.file_folders()
-        sw = self.open_status_writer(status_f)
 
         preds = self.read_predictions(test_df, ind_pred_f, rel_pred_f)
         modified_df = self.read_modified(data_f) if modified else None
@@ -34,19 +33,16 @@ class Evaluation:
             pred_df, col, name, line = pred
             save = True if pred[1] in preds[-1][1] else False
             scores = self.merge_and_score(test_df, pred, fname, save,
-                                          modified_df, sw)
+                                          modified_df)
             score_dict[name] = scores
-        self.util_obj.close_writer(sw)
         return score_dict
 
     # private
     def settings(self):
-        """Sets the noise limit added to predictions."""
-        noise_limit = 0.0025
+        noise_limit = 0.000025
         self.util_obj.set_noise_limit(noise_limit)
 
     def file_folders(self):
-        """Returns absolute path directories."""
         ind_dir = self.config_obj.ind_dir
         rel_dir = self.config_obj.rel_dir
         domain = self.config_obj.domain
@@ -63,21 +59,7 @@ class Evaluation:
             os.makedirs(status_f)
         return ind_data_f, ind_pred_f, rel_pred_f, rel_image_f, status_f
 
-    def open_status_writer(self, status_f):
-        """Opens a file object to write evaluation status to.
-        status_f: status folder.
-        Returns file object."""
-        fold = self.config_obj.fold
-        fname = status_f + 'eval_' + fold + '.txt'
-        f = self.util_obj.open_writer(fname)
-        return f
-
     def read_predictions(self, test_df, ind_pred_f, rel_pred_f, dset='test'):
-        """Reads in the independent and relational model predictions.
-        ind_pred_f: independent model predictions folder.
-        rel_pred_f: relational model predictions folder.
-        dset: dataset to read.
-        Returns dataframes for the predictions."""
         fold = self.config_obj.fold
         util = self.util_obj
         fname = dset + '_' + fold
@@ -100,14 +82,7 @@ class Evaluation:
         return preds
 
     def merge_and_score(self, test_df, pred, fname, save=False,
-                        modified_df=None, fw=None):
-        """Merges the predictions onto the test set and computes the
-                evaluation metrics.
-        test_df: test set dataframe.
-        pred: tuple with prediction dataframe, col, name, and line pattern.
-        fname: name of the file to save the pr curve.
-        save: boolean to save pr curve or not.
-        modified_df: if true, take out relabeled instances."""
+                        modified_df=None):
         pred_df, col, name, line = pred
 
         merged_df = self.merge_predictions(test_df, pred_df)
@@ -117,7 +92,7 @@ class Evaluation:
 
         noise_df = self.apply_noise(merged_df, col)
         pr, roc, r, p, npr = self.compute_scores(noise_df, col)
-        self.print_scores(name, pr, roc, npr, fw=fw)
+        self.print_scores(name, pr, roc, npr)
         # self.util_obj.plot_pr_curve(name, fname, r, p, npr, line=line,
         #         save=save)
         scores = {'aupr': round(pr, 7), 'auroc': round(roc, 7),
@@ -125,44 +100,24 @@ class Evaluation:
         return scores
 
     def merge_predictions(self, test_df, pred_df):
-        """Merges the independent and relational dataframes together.
-        test_df: testing dataframe.
-        pred_df: predictions dataframe.
-        Returns merged dataframe."""
         merged_df = test_df.merge(pred_df, on='com_id', how='left')
         return merged_df
 
     def read_modified(self, data_f):
-        """Read in the predictions for the adversarial comments.
-        pred_f: predictions folder.
-        Returns dataframe of identified adversarial comments, dataframe of
-                false positives."""
         fname = data_f + 'labels.csv'
         if self.util_obj.check_file(fname):
             modified_df = pd.read_csv(fname)
         return modified_df
 
     def filter(self, merged_df, modified_df):
-        """Only keep the relational predictions for those in helped_df or
-                hurt_df dataframes.
-        df: comments dataframe with independent predictions.
-        helped_df: helped dataframe with relational predictions.
-        hurt_df: hurt dataframe with relational predictions.
-        Returns dataframe with an altered relational predictions column."""
         temp_df = merged_df[~merged_df['com_id'].isin(modified_df['com_id'])]
         return temp_df
 
     def apply_noise(self, merged_df, col):
-        """Adds a small amount of noise to each prediction for both models.
-        merged_df: testing dataframe with merged predictions."""
         merged_df[col] = merged_df[col].apply(self.util_obj.gen_noise)
         return merged_df
 
     def compute_scores(self, pf, col):
-        """Computes the precision, recall, aupr, and auroc scores.
-        pf: dataframe with the merged predictions.
-        col: column identifer for predictions to compare to the labels.
-        Returns aupr, auroc, recalls, precisions, and neg-aupr scores."""
         fpr, tpr, _ = roc_curve(pf['label'], pf[col])
         prec, rec, _ = precision_recall_curve(pf['label'], pf[col])
         nPre, nRec, _ = precision_recall_curve(pf['label'], 1 - pf[col],
@@ -171,10 +126,5 @@ class Evaluation:
         return aupr, auroc, rec, prec, nAupr
 
     def print_scores(self, name, aupr, auroc, naupr, fw=None):
-        """Print the scores to stdout.
-        name: name of the model.
-        aupr: area under the pr curve.
-        auroc: area under the roc curve.
-        naupr: neg-aupr."""
         s = name + ' evaluation...AUPR: %.4f, AUROC: %.4f, N-AUPR: %.4f' + '\n'
         self.util_obj.write(s % (aupr, auroc, naupr), fw=fw)
